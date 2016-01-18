1839cfcc27b7686c1b008e9f9adb1ef6e8256426
Tomas <tomasrogberg@gmail.com>
2016-01-18 12:25:30 +0100

    NewInit migration

--- Garage2/Migrations/201601181124556_NewInit.Designer.cs
+++ Garage2/Migrations/201601181124556_NewInit.Designer.cs
@@ -7,13 +7,13 @@ namespace Garage2.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.1-30610")]
-    public sealed partial class InitialCreate : IMigrationMetadata
+    public sealed partial class NewInit : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(InitialCreate));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(NewInit));
         
         string IMigrationMetadata.Id
         {
-            get { return "201601180834234_InitialCreate"; }
+            get { return "201601181124556_NewInit"; }
         }
         
         string IMigrationMetadata.Source
